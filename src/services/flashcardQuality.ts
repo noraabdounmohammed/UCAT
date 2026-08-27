51303511e0fe65348f989aac2c391461eb492bfa
noraabdounmohammed <49647318+noraabdounmohammed@users.noreply.github.com>
2026-08-27 12:27:52 +0100

    Expose executable flashcard QA service

---
+++ src/services/flashcardQuality.ts
@@ -0,0 +1,7 @@
+export {
+  FLASHCARD_QUALITY_GATE_VERSION,
+  FLASHCARD_QUALITY_INSTRUCTIONS,
+  validateFlashcard,
+  reviewFlashcard,
+} from './flashcardQualityV2';
+export type { FlashcardQualityResult } from './flashcardQualityV2';
